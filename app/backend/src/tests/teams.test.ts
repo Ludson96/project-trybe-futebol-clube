22e689d9160dc0b93ee229c0bb9235f19b41f0bf
Ludson96 <ludson_ps27@hotmail.com>
2023-01-26 15:27:30 -0300

    tests: MSC getTeamById

--- app/backend/src/tests/teams.test.ts
+++ app/backend/src/tests/teams.test.ts
@@ -6,7 +6,7 @@ import chaiHttp = require('chai-http');
 import { Response } from 'superagent';
 import { app } from '../app';
 import TeamsModel from '../database/models/TeamsModel';
-import { allTeams } from './mocks/teams.mock';
+import { allTeams, teamId } from './mocks/teams.mock';
 
 chai.use(chaiHttp);
 
@@ -28,8 +28,9 @@ describe('Teste Teams', function() {
 
       expect(chaiHttpResponse.status).to.be.equal(200);
       expect(chaiHttpResponse.body).to.deep.equal(allTeams);
-    }); 
+    });
 
+    // https://www.coreycleary.me/faking-errors-to-test-error-scenarios-in-express-apis/
     it('internal Error', async () => {
       sinon
         .stub(TeamsModel, 'findAll')
@@ -43,108 +44,40 @@ describe('Teste Teams', function() {
       expect(chaiHttpResponse.status).to.be.equal(500);
       expect(chaiHttpResponse.body).to.deep.equal('db query failed');
     });
+  });
 
-  //   it('Não é possível realizar login sem senha', async () => {
-  //     sinon
-  //     .stub(UserModel, "findOne")
-  //     .resolves(validUser as UserModel);
+  describe('Retorna um time especifico pelo id', function() {
+    let chaiHttpResponse: Response;
 
-  //     chaiHttpResponse = await chai
-  //     .request(app)
-  //     .post('/login')
-  //     .send(withoutPwd);
+    afterEach(sinon.restore);
 
-  //     expect(chaiHttpResponse.status).to.be.equal(400);
-  //     expect(chaiHttpResponse.body).to.deep.equal({ "message": "All fields must be filled" })
-  //   });
+    it('com sucesso', async () => {
+      sinon
+      .stub(TeamsModel, "findOne")
+      .resolves(teamId as TeamsModel);
 
-  //   it('Não é possível realizar login com um email incorreto', async () => {
-  //     sinon
-  //     .stub(UserModel, "findOne")
-  //     .resolves();
+      chaiHttpResponse = await chai
+      .request(app)
+      .get('/teams/:id');
 
-  //     chaiHttpResponse = await chai
-  //     .request(app)
-  //     .post('/login')
-  //     .send(invalidEmail);
-
-  //     expect(chaiHttpResponse.status).to.be.equal(401);
-  //     expect(chaiHttpResponse.body).to.deep.equal({ "message": "Incorrect email or password" })
-  //   }); 
-
-  //   it('Não é possível realizar login com a senha incorreto', async () => {
-  //     sinon
-  //     .stub(UserModel, "findOne")
-  //     .resolves(invalidPwd as UserModel);
-
-  //     chaiHttpResponse = await chai
-  //     .request(app)
-  //     .post('/login')
-  //     .send(invalidPwd);
-
-  //     expect(chaiHttpResponse.status).to.be.equal(401);
-  //     expect(chaiHttpResponse.body).to.deep.equal({ "message": "Incorrect email or password" })
-  //   });
-  // });
-
-  // describe('Teste para o getRole', function() {
-  //   let chaiHttpResponse: Response;
-
-  //   afterEach(sinon.restore);
-
-  //   it('Retorna a role corretamente', async () => {
-  //     sinon
-  //     .stub(UserModel, "findOne")
-  //     .resolves(validUser as UserModel);
-
-  //     chaiHttpResponse = await chai
-  //     .request(app)
-  //     .get('/login/validate')
-  //     .set({Authorization: token});
-
-  //     expect(chaiHttpResponse.status).to.be.equal(200);
-  //     expect(chaiHttpResponse.body).to.deep.equal({ "role": "user" })
-  //   });
+      expect(chaiHttpResponse.status).to.be.equal(200);
+      expect(chaiHttpResponse.body).to.deep.equal(teamId)
+    });
     
-  //   it('Email incorreto', async () => {
-  //     sinon
-  //     .stub(UserModel, "findOne")
-  //     .resolves();
+    it('internal Error', async () => {
+      sinon
+        .stub(TeamsModel, "findByPk")
+        .throws(Error('db query failed'))
+  
 
-  //     chaiHttpResponse = await chai
-  //     .request(app)
-  //     .get('/login/validate')
-  //     .set({Authorization: token});
+      chaiHttpResponse = await chai
+        .request(app)
+        .get('/teams/:id');
 
-  //     expect(chaiHttpResponse.status).to.be.equal(401);
-  //     expect(chaiHttpResponse.body).to.deep.equal({ message: 'email incorreto' })
-  //   });
+      expect(chaiHttpResponse.status).to.be.equal(500);
+      expect(chaiHttpResponse.body).to.deep.equal('db query failed');
+    });
 
-  //   it('Token invalido', async () => {
-  //     sinon
-  //     .stub(UserModel, "findOne")
-  //     .resolves();
 
-  //     chaiHttpResponse = await chai
-  //     .request(app)
-  //     .get('/login/validate')
-  //     .set({Authorization: 'token'});
-
-  //     expect(chaiHttpResponse.status).to.be.equal(401);
-  //     expect(chaiHttpResponse.body).to.deep.equal({ message: 'Invalid token' })
-  //   });
-
-  //   it('Token inexistente', async () => {
-  //     sinon
-  //     .stub(UserModel, "findOne")
-  //     .resolves();
-
-  //     chaiHttpResponse = await chai
-  //     .request(app)
-  //     .get('/login/validate')
-
-  //     expect(chaiHttpResponse.status).to.be.equal(401);
-  //     expect(chaiHttpResponse.body).to.deep.equal({ message: 'token not found' })
-  //   });
   });
 });
